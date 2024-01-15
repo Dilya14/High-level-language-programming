0e3c4a41c75b140c016c1cdad7e729723a340469
Dilnoza <98981169+Dilya14@users.noreply.github.com>
2024-01-15 23:20:15 +0800

    Add files via upload

---
+++ 3-b2-2.cpp
@@ -0,0 +1,26 @@
+﻿/*2056011 计科 李书瑶*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter an integer between [1..30000]: \n";
+	int num;
+	cin >> num;
+
+	int a, b, c, d, e;
+
+	a = num / 10000;
+	b = (num / 1000) % 10 ;
+	c = (num / 100) % 10;
+	d = (num / 10) % 10;
+	e = num % 10;
+
+	cout << "Tens of Thousands: " << a << endl;
+	cout << "Thousands:         " << b << endl;
+	cout << "Hundreds:          " << c << endl;
+	cout << "Tens:              " << d << endl;
+	cout << "Units:             " << e << endl;
+
+	return 0;
+}
